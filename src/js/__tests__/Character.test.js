46e614e22df31bd2325aade446bd18dcfcd94c71
Stanislav <S.tanislav-Z.abrodin@yandex.ru>
2021-09-28 13:41:27 +0400

    2.1

--- src/js/__tests__/Character.test.js
+++ src/js/__tests__/Character.test.js
@@ -1,11 +1,11 @@
 import Character from '../app';
 
 test('create Character name < 2', () => {
-  expect(() => new Character('a', 'Bowman')).toThrowError(new Error('Имя должно быть не менее 2 и не более 10 символов'));
+  expect(() => new Character('a', 'Bowman')).toThrowError(new Error('Слишком много/мало символов'));
 });
 
 test('create Character name > 10', () => {
-  expect(() => new Character('a1234567890', 'Bowman')).toThrowError(new Error('Имя должно быть не менее 2 и не более 10 символов'));
+  expect(() => new Character('a1234567890', 'Bowman')).toThrowError(new Error('Слишком много/мало символов'));
 });
 
 test('create Character type Error', () => {
--- src/js/app.js
+++ src/js/app.js
@@ -1,91 +1,79 @@
 export default class Character {
 	constructor(name, type)	{
-		if (name.length()<2 || name.length()>10){
+		if (name.length<2 || name.length>10){
 			throw new Error('Слишком много/мало символов')
 		}
 		else{
 			this.name = name;
 		}
-
-
 		const types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
-    	if (types.indexOf(type) === -1) {
-      		throw new Error('Такого класса нет');
-    	} 
-    	else {
-      		this.type = type;
-    	}
+		if (types.indexOf(type) === -1) {
+			throw new Error('Такого класса нет');
+		} 
+		else {
+			this.type = type;
+		}
 		this.health = 100;
 		this.level = 1;
-		this.attack = 25;
-    	this.defence = 25;
-
-
-		levelUp() {
-	    	if (this.health > 0) {
-		        this.level += 1;
-		        this.attack *= 1.2;
-		        this.defence *= 1.2;
-		        this.health = 100;
-		    }
-		    else {
-		      throw new Error('Нельзя повысить левел умершего');
-		    }
-  		}
-
-		damage(points) {
-			this.health -= points * (1 - this.defence / 100);
-		    if (this.health < 0) {
-		      this.health = 0;
-		    }
+	}
+	levelUp() {
+		if (this.health > 0) {
+			this.level += 1;
+			this.attack *= 1.2;
+			this.defence *= 1.2;
+			this.health = 100;
+		}
+		else {
+			throw new Error('Нельзя повысить левел умершего');
+		}
+	}
+	damage(points) {
+		this.health -= points * (1 - this.defence / 100);
+		if (this.health < 0) {
+		this.health = 0;
 		}
 	}
 }
-
 class Bowerman extends Character {
 	constructor(name, type) {
-    	super(name, type);
+		super(name, type);
+		this.attack = 25;
+		this.defence = 25;
 	}
-	this.attack = 25;
-	this.defence = 25;
 }
-
 class Swordsman extends Character {
 	constructor(name, type) {
-    	super(name, type);
+		super(name, type);
+		this.attack = 40;
+		this.defence = 10;
 	}
-	this.attack = 40;
-	this.defence = 10;
 }
-
 class Magician extends Character {
 	constructor(name, type) {
-    	super(name, type);
+		super(name, type);
+		this.attack = 10;
+		this.defence = 40;
 	}
-	this.attack = 10;
-	this.defence = 40;
 }
-
 class Daemon extends Character {
 	constructor(name, type) {
-    	super(name, type);
+		super(name, type);
+		this.attack = 10;
+		this.defence = 40;
 	}
-	this.attack = 10;
-	this.defence = 40;
 }
 
 class Undead extends Character {
 	constructor(name, type) {
-    	super(name, type);
+		super(name, type);
+		this.attack = 25;
+		this.defence = 25;
 	}
-	this.attack = 25;
-	this.defence = 25;
 }
-
 class Zombie extends Character {
 	constructor(name, type) {
-    	super(name, type);
-	}		
-	this.attack = 40;
-	this.defence = 10;
+		super(name, type);	
+		this.attack = 40;
+		this.defence = 10;
+	}
 }
